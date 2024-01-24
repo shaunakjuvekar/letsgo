36db98793c9a9cd8dde0044759196fbde8c19d4e
Shaunak <gamershaunj@gmail.com>
2024-01-24 15:08:32 -0500

    removed folder

--- letsgo/backend/src/routes/report.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const router = Router()
-import { findAll, create } from '../controllers/report.controller.js';
-
-router.get('/', findAll);
-router.post('/', create);
-
-export default router
